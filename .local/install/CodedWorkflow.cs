a0723550494f1e44465dfc309955dda84c4240fc
“ILIIH” <jointlord0@gmail.com>
2023-06-29 00:17:08 +0100

    TP-14 added response send robot

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace TripPlanner
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
